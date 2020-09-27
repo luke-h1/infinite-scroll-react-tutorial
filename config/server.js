b463b848cfc35042dc07956bf6f94b20841d61a5
luke-h1 <luke.howsam@yahoo.com>
2020-09-27 12:37:20 +0100

    update gitignore

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = {
   APPLICATION_ID:
-    process.env.APPLICATION_ID || '7IPEqyfXWcjNLENa9FHbTtqUKR27WKK9pPOCrezNu4w',
-  SECRET: process.env.SECRET || '6S_KeobivJcoIJj-clsdkQG4-JxQQhvlxpfTXbEm7GE',
+    process.env.APPLICATION_ID || 'xxxxxxxxxxxxx',
+  SECRET: process.env.SECRET || 'xxxxxxxxxxxxx',
   CALLBACK_URL: process.env.CALLBACK_URL || 'http://localhost:3000',
 };
